bd607432dd0d0430d2d8ee5b20186780838b59f6
Richard <richarddirven@outlook.com>
2021-08-15 16:48:02 +0200

    Add build script for local debugging

---
+++ build.sh
@@ -0,0 +1,4 @@
+sass --no-source-map assets/css/scss/main.scss assets/css/main.css
+sass --no-source-map assets/css/scss/light.scss assets/css/light.css
+sass --no-source-map assets/css/scss/dark.scss assets/css/dark.css
+bundle exec jekyll serve
